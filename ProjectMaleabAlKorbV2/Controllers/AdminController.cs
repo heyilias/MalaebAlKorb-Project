2e7e3a1413aa543f12e36383fed12a5a31c0a40a
unknown <ilias7576@gmail.com>
2020-05-18 03:05:05 +0000

    Integrate Admin page

---
+++ ProjectMaleabAlKorbV2/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectMaleabAlKorbV2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registers()
+        {
+            return View();
+        }
+
+        public ActionResult Reservations()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
