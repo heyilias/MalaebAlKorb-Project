b6d5eeb5da9c5a2fec852b5eea20d6a7bdaf7834
unknown <ilias7576@gmail.com>
2020-05-19 03:05:15 +0000

    Page Register (Affichage des info & Ajout Player)

--- ProjectMaleabAlKorbV2/Controllers/AdminController.cs
+++ ProjectMaleabAlKorbV2/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProjectMaleabAlKorbV2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace ProjectMaleabAlKorbV2.Controllers
 {
     public class AdminController : Controller
     {
+        MalaebAlKorbEntities db = new MalaebAlKorbEntities();
         // GET: Admin
         public ActionResult Index()
         {
@@ -17,6 +19,34 @@ namespace ProjectMaleabAlKorbV2.Controllers
         public ActionResult Registers()
         {
             return View();
+        }
+
+        //Show all players in table registers
+        public JsonResult GetPlyarsList()
+        {
+            List<Player> playerList = db.Players.ToList<Player>();
+
+            return Json(playerList, JsonRequestBehavior.AllowGet);
+        }
+
+        //ADD PLAYERS
+        public JsonResult SaveDataInDatabase(Player model)
+        {
+            var result = false;
+            if (ModelState.IsValid)
+                try
+                {
+                    model.dateCreated = DateTime.Now;
+                    db.Players.Add(model);
+                    db.SaveChanges();
+                    result = true; 
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Reservations()
